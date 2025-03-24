2ff2ff20e1aeed814df9c09d7845c1c5ba7dd21d
Ricky Winowiecki <slikstir@gmail.com>
2025-03-24 20:33:11 +1100

    Cache Store using Redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,13 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :solid_cache_store
-
+  config.cache_store = :redis_cache_store, {
+    url: ENV['REDIS_URL'],
+    error_handler: -> (method:, returning:, exception:) {
+      Rails.logger.error "Redis cache error: #{exception}"
+    }
+  }
+  
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
